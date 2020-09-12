c0a3427d0882103ef2941c73d03e912b8c82edf9
ABDULLAH ALL MAMUN <32321938+mamun9ey@users.noreply.github.com>
2020-09-12 12:55:17 +0600

    Updates

---
+++ precache-manifest.50c9d48b74b5a5cca97242e64199ec0a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46231dfc294a17917589f4807bf6c11",
+    "url": "/Udemy/index.html"
+  },
+  {
+    "revision": "80e8f4b21de99dd6ad0b",
+    "url": "/Udemy/static/css/2.894a1137.chunk.css"
+  },
+  {
+    "revision": "dcd11ac60352ea2769ae",
+    "url": "/Udemy/static/css/main.849fde7f.chunk.css"
+  },
+  {
+    "revision": "80e8f4b21de99dd6ad0b",
+    "url": "/Udemy/static/js/2.905e1c64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Udemy/static/js/2.905e1c64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcd11ac60352ea2769ae",
+    "url": "/Udemy/static/js/main.5e84581a.chunk.js"
+  },
+  {
+    "revision": "091fdcdf541bf00ec2fd",
+    "url": "/Udemy/static/js/runtime-main.9efd751e.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/Udemy/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/Udemy/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/Udemy/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/Udemy/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/Udemy/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "a80cb03c76a564cb2848f2e1a79ec30f",
+    "url": "/Udemy/static/media/udemy.a80cb03c.jpg"
+  }
+]);
